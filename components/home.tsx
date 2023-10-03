f9adfe3e30bab0c00d9de9be6622b88ec93899a0
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2023-10-03 15:23:29 +0330

    refactor: update home

--- components/home.tsx
+++ components/home.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const socials = [
   {
     title: "GitHub",
@@ -11,35 +14,63 @@ const socials = [
     title: "Twitter",
     url: "https://twitter.com/behzad_pro",
   },
+  {
+    title: "Instagram",
+    url: "https://instagram.com/behzad.pro?igshid=OGQ5ZDc2ODk2ZA==",
+  },
 ];
 
 export default function Home() {
   return (
-    <header className="bg-gainsboro min-h-screen relative">
-      <section className="container mx-auto max-w-screen-lg pt-10 px-4">
-        <h1 className="tk-ivypresto-display text-6xl md:text-8xl flex flex-col tracking-wide">
-          <span>Behzad</span>
+    <header className="min-h-screen relative bg-neutral-900 place-content-center grid">
+      <figure>
+        <div className="grid grid-flow-col items-center">
+          <h1 className="tk-ivypresto-display text-neutral-100 text-6xl md:text-8xl flex flex-col tracking-wide">
+            <span>Behzad</span>
+          </h1>
+          <Image
+            src="/assets/avatar.jpeg"
+            width={80}
+            height={80}
+            alt="Behzad Ali Mohammad Zad"
+            className="rounded-full drop-shadow-xl grayscale opacity-30 hover:grayscale-0 hover:opacity-100 transition-all"
+          />
+        </div>
+        <h2 className="tk-ivypresto-display text-6xl md:text-8xl flex flex-col tracking-wide">
+          {" "}
           <span className="text-sm md:text-lg tk-ivypresto-display">
-            <span className="text-neutral-400">//</span>
-            <span>Ali Mohammad Zad</span>
-            <span className="text-neutral-400">//</span>
-            <span className="tk-brother-1816 text-xs">
+            <span className="text-neutral-700">//</span>
+            <span className="text-neutral-400">Ali Mohammad Zad</span>
+            <span className="text-neutral-700">//</span>
+            <span className="tk-brother-1816 text-xs text-neutral-400">
               Senior Software Engineer
             </span>
           </span>
-        </h1>
-        <nav className="tk-brother-1816 text-xs space-x-4 transition-colors delay-200 mt-2">
-          {socials.map((item) => (
-            <a
-              href={item.url}
-              className="text-amber-800 hover:text-blue-900 underline underline-offset-4"
-              target="_blank"
-            >
-              {item.title}
-            </a>
-          ))}
-        </nav>
-      </section>
+        </h2>
+      </figure>
+      <nav className="tk-brother-1816 text-xs space-x-4 transition-colors delay-200 mt-2">
+        {socials.map((item) => (
+          <a
+            key={`nav-${item.title}`}
+            href={item.url}
+            className="text-green-400 hover:text-green-500 underline underline-offset-4"
+            target="_blank"
+          >
+            {item.title}
+          </a>
+        ))}
+        <Link
+          className="text-yellow-400 hover:text-yellow-500 underline underline-offset-4"
+          href="/blog"
+        >
+          Blog
+        </Link>
+      </nav>
+      <p className="max-w-md my-4 text-neutral-400 leading-relaxed text-sm">
+        I am software engineer with 10 years of professional experience
+        developing end-to-end software solutions, primarily in front-end
+        engineering. Also, I am a hiker and photography lover.
+      </p>
     </header>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 
 export default function Index() {
   return (
-    <Layout>
+    <Layout dir="ltr">
       <Home />
     </Layout>
   );
